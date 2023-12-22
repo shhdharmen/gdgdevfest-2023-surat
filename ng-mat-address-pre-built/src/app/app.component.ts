092d4ecd8c2106b92ad5535e7b2d45db00ad5536
Dharmen Shah <shhdharmen@gmail.com>
2023-12-22 16:12:38 +0530

    complete examples

---
+++ ng-mat-address-pre-built/src/app/app.component.ts
@@ -0,0 +1,69 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { AddressFormComponent } from './forms/address-form/address-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    AddressFormComponent,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'ng-mat-address-pre-built';
+  private document = inject(DOCUMENT);
+  private isDark = false;
+  readonly DARK_THEME = 'https://material.angular.io/pink-bluegrey.css';
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    this.document.documentElement.classList.toggle('dark-theme');
+    if (this.isDark) {
+      const href = this.DARK_THEME;
+      this.getLinkElementForKey('dark-theme').setAttribute('href', href);
+    } else {
+      this.removeStyle('dark-theme');
+    }
+  }
+
+  getLinkElementForKey(key: string) {
+    return (
+      this.getExistingLinkElementByKey(key) ||
+      this.createLinkElementWithKey(key)
+    );
+  }
+
+  getExistingLinkElementByKey(key: string) {
+    return document.head.querySelector(
+      `link[rel="stylesheet"].${this.getClassNameForKey(key)}`
+    );
+  }
+
+  createLinkElementWithKey(key: string) {
+    const linkEl = document.createElement('link');
+    linkEl.setAttribute('rel', 'stylesheet');
+    linkEl.classList.add(this.getClassNameForKey(key));
+    document.head.appendChild(linkEl);
+    return linkEl;
+  }
+
+  getClassNameForKey(key: string) {
+    return `style-manager-${key}`;
+  }
+
+  removeStyle(key: string) {
+    const existingLinkElement = this.getExistingLinkElementByKey(key);
+    if (existingLinkElement) {
+      this.document.head.removeChild(existingLinkElement);
+    }
+  }
+}
---
+++ ng-mat-address/src/app/forms/address-form/address-form.component.ts
@@ -0,0 +1,57 @@
+import { Component, Input, inject } from '@angular/core';
+
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-address-form',
+  templateUrl: './address-form.component.html',
+  styleUrls: ['./address-form.component.scss'],
+  standalone: true,
+  imports: [
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    ReactiveFormsModule,
+  ],
+})
+export class AddressFormComponent {
+  private fb = inject(FormBuilder);
+  addressForm = this.fb.group({
+    company: null,
+    firstName: [null, Validators.required],
+    lastName: [null, Validators.required],
+    address: [null, Validators.required],
+    address2: null,
+    city: [null, Validators.required],
+    state: [null, Validators.required],
+    postalCode: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(5),
+      ]),
+    ],
+    shipping: ['free', Validators.required],
+  });
+
+  hasUnitNumber = false;
+
+  states = [
+    { name: 'Gujarat', abbreviation: 'GJ' },
+    { name: 'Maharashtra', abbreviation: 'MH' },
+  ];
+
+  onSubmit(): void {
+    alert('Thanks!');
+  }
+
+  @Input() addressHeader = 'Shipping Information';
+}
